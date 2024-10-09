9dde2a662bb60f173d62c0519c4abc55e8e02200
David <dav.baz@alice.it>
2024-10-09 22:05:26 +0200

    Configure individual authors for sniper_main.py

--- sniper_main.py
+++ sniper_main.py
@@ -11,22 +11,121 @@ if __name__ == "__main__":
     )
     
     # Configure individual authors
-    sniper.configure_author("fabi.one", 
+    #DEFAULT_AUTHORS = ["maodomy","gue22","aston.villa","Kork75","mia.fobos", "im-ridd", "tasubot", "stefano.massari", "wildnature1", "mikitaly", "guersy", "frafiomatale", "cur8"]
+
+    sniper.configure_author("maodomy", 
                             vote_percentage=75, 
                             post_delay_minutes=1, 
                             daily_vote_limit=30, 
                             add_comment=True, 
                             add_image=True,
-                            comment_text="Great post!",
+                            comment_text="Bel post!",
                             image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
     
     # Configure individual authors
-    sniper.configure_author("gigi.one", 
+    sniper.configure_author("gue22", 
                             vote_percentage=75, 
                             post_delay_minutes=1, 
                             daily_vote_limit=30, 
-                            add_comment=False, 
-                            add_image=False)
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
+    sniper.configure_author("aston.villa", 
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=False,
+                            comment_text="Bel post!")
+    
+    sniper.configure_author("Kork75", 
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=False,
+                            comment_text="Bel post!")
+    
+    sniper.configure_author("mia.fobos",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
+    sniper.configure_author("im-ridd",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
+    sniper.configure_author("tasubot",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
+    sniper.configure_author("stefano.massari",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=False,
+                            comment_text="Bel post!")
+    
+    sniper.configure_author("wildnature1",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
+    sniper.configure_author("mikitaly",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=False,
+                            comment_text="Bel post!")
+    
+    sniper.configure_author("guersy",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
+    sniper.configure_author("frafiomatale",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=False,
+                            comment_text="Bel post!")
+    
+    sniper.configure_author("cur8",
+                            vote_percentage=75, 
+                            post_delay_minutes=1, 
+                            daily_vote_limit=30, 
+                            add_comment=True, 
+                            add_image=True,
+                            comment_text="Bel post!",
+                            image_path="_0d47c611-0fa1-4df6-898c-b6620812e18d.jpeg")
+    
     
     sniper.start()
     # To stop the sniper after some time:
